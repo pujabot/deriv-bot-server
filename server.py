135074145d4732c7ec16f0025769bc3cc1745ccf
pujabot <silviakelly110398@gmail.com>
2026-03-30 17:36:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 import subprocess
 
 app = Flask(__name__)
-
 CORS(app)
 
 bot_process = None
@@ -16,35 +15,28 @@ def home():
 
 @app.route("/start", methods=["POST"])
 def start_bot():
-
     global bot_process
 
+    data = request.json
+    token = data["token"]
+
     if bot_process is None:
-
-        bot_process = subprocess.Popen(["python","bot.py"])
-
-        return jsonify({"status":"bot started"})
-
+        bot_process = subprocess.Popen(["python", "bot.py", token])
+        return jsonify({"status": "bot started"})
     else:
-
-        return jsonify({"status":"bot already running"})
+        return jsonify({"status": "bot already running"})
 
 
 @app.route("/stop", methods=["POST"])
 def stop_bot():
-
     global bot_process
 
     if bot_process:
-
         bot_process.terminate()
         bot_process = None
-
-        return jsonify({"status":"bot stopped"})
-
+        return jsonify({"status": "bot stopped"})
     else:
-
-        return jsonify({"status":"bot not running"})
+        return jsonify({"status": "bot not running"})
 
 
 app.run(host="0.0.0.0", port=10000)
