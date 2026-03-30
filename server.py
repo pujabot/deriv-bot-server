8edf8dc5e264175971f550fcaffe154d034f1481
pujabot <silviakelly110398@gmail.com>
2026-03-30 16:08:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,35 +1,47 @@
 from flask import Flask, request, jsonify
+import subprocess
 
 app = Flask(__name__)
 
-running = False
+bot_process = None
+
+
+@app.route("/")
+def home():
+    return "Bot server running"
+
 
 @app.route("/start", methods=["POST"])
 def start_bot():
 
-    global running
-    running = True
+    global bot_process
 
-    print("Bot started")
+    if bot_process is None:
 
-    return jsonify({"status":"bot started"})
+        bot_process = subprocess.Popen(["python","bot.py"])
+
+        return jsonify({"status":"bot started"})
+
+    else:
+
+        return jsonify({"status":"bot already running"})
 
 
 @app.route("/stop", methods=["POST"])
 def stop_bot():
 
-    global running
-    running = False
+    global bot_process
 
-    print("Bot stopped")
+    if bot_process:
 
-    return jsonify({"status":"bot stopped"})
+        bot_process.terminate()
+        bot_process = None
+
+        return jsonify({"status":"bot stopped"})
+
+    else:
+
+        return jsonify({"status":"bot not running"})
 
 
-@app.route("/")
-def home():
-
-    return "Bot server running"
-
-
-app.run(host="0.0.0.0",port=10000)
+app.run(host="0.0.0.0", port=10000)
